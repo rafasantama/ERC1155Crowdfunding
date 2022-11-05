648f5ed8280d6945ca5ce18ab4b73cf9b4d9663e
Rafael SantaMaría Isaza <rafasantama@gmail.com>
2022-11-05 12:31:20 -0500

    update struct para proyectos

--- contract/ERC1155Crowdfunding.sol
+++ contract/ERC1155Crowdfunding.sol
@@ -10,28 +10,66 @@ import "@openzeppelin/contracts@4.7.3/token/ERC1155/utils/ERC1155Holder.sol";
 import "@openzeppelin/contracts@4.7.3/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts@4.4.1/security/ReentrancyGuard.sol";
 
+
+//Set TimeLock (F)
+//Start - End Porjects
+//KYC LIST
+// EVENTS OF PROJECTS
+// AUTOMATIC ID (ADDRESS, FUNCTIONS)
+// CheckID EXIST
+
+// DONT OVERRIDE PROJECT
+
+// URI FOR PROJECTS
+
+// Distribucion Justa
+
+
+
 /// @custom:security-contact info@bitsdapps.tech
-contract ProjectCrowdfunding is ReentrancyGuard, ERC1155,ERC1155Holder, AccessControl, Pausable, ERC1155Burnable, ERC1155Supply {
+    contract ProjectCrowdfunding is 
+    ReentrancyGuard,
+    ERC1155,
+    ERC1155Holder,
+    AccessControl,
+    Pausable,
+    ERC1155Burnable,
+    ERC1155Supply
+    
+    {
+    
     bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    mapping (uint=>uint) public projectPrice;
-    mapping (uint => mapping(address => uint256)) public projectOwnerShares;
-    mapping (uint => uint) public projectSales;
-    mapping (uint => uint) public projectProfit;
-    mapping (address => bool) public addressProjectHolder;
-    mapping (uint =>address[]) public projectHolders;
-    mapping (uint =>uint) public projectHoldersCount;
+    bytes32 public constant PROJECTS_ROLE = keccak256("PROJECTS_ROLE");
 
-    constructor(string memory tokenURI) ERC1155(tokenURI) {
+     struct Project {
+        uint256 projectPrice;
+        uint256 projectSales;
+        address payable[] projectHolders;
+        uint256 projectProfit;
+        uint256 projectTotalSupply;
+        mapping (address => uint256) projectOwnerShares; //mapping (uint256 => address) projectOwnerShares;
+        mapping (address => bool) addressProjectHolder;
+        mapping (address => uint256) ProfitGainsByOwner; 
+        string projectURI;
+    }
+        mapping (uint256 => Project) projectStructs;
+        mapping (uint256 => string) projectURI;
+        Project[] public projectsData;
+        uint256 public totalProjects;
+
+        constructor(string memory tokenURI) ERC1155(tokenURI) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(URI_SETTER_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
-        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(PROJECTS_ROLE, msg.sender);
     }
 
-    function setURI(string memory newuri) public onlyRole(URI_SETTER_ROLE) {
-        _setURI(newuri);
+    function setURI(string memory _project_URI, uint256 _id) public onlyRole(URI_SETTER_ROLE) {
+        projectStructs[_id].projectURI = _project_URI;
+    }
+    function uri(uint256 _id) public view virtual override returns(string memory) {
+        return (projectStructs[_id].projectURI);
     }
 
     function pause() public onlyRole(PAUSER_ROLE) {
@@ -42,40 +80,161 @@ contract ProjectCrowdfunding is ReentrancyGuard, ERC1155,ERC1155Holder, AccessCo
         _unpause();
     }
 
-    function mintSell(uint256 id, uint256 amount, bytes memory data, uint256 price)
+
+        //Overrading mintSell
+        //********NOTA: implementar mecanismo para cancelar proyectos con errores
+        function NewProject(uint256 amount,
+        uint256 price, string memory project_URI) public
+        onlyRole(PROJECTS_ROLE)
+        {
+        totalProjects++;
+        _mint(address(this), totalProjects, amount,"");
+        projectStructs[totalProjects].projectPrice = price;
+        projectStructs[totalProjects].projectTotalSupply = amount;
+        projectStructs[totalProjects].projectURI = project_URI;
+        }
+
+        function getTotalProjectById(uint256 _id) public view
+        returns(
+        uint256,
+        uint256,
+        address payable[] memory,
+        uint256,
+        uint256
+        ){
+            return(
+            projectStructs[_id].projectPrice,
+            projectStructs[_id].projectSales,
+            projectStructs[_id].projectHolders,
+            projectStructs[_id].projectProfit,
+            projectStructs[_id].projectTotalSupply
+            );
+        }
+        
+
+        function buyProject2
+        (uint256 _id)
         public
-        onlyRole(MINTER_ROLE)
-    {
-        _mint(address(this), id, amount, data);
-        projectPrice[id]=price;
-    }
-    function buyProject(uint256 id) public payable nonReentrant(){
-        require(msg.value>=projectPrice[id], "not enough for single token");
-        if(msg.value>projectPrice[id]){
-            _safeTransferFrom(address(this),_msgSender(),id,msg.value/projectPrice[id],'0x0');
-            projectOwnerShares[id][_msgSender()]+=msg.value/projectPrice[id];
+        payable
+        nonReentrant()
+        {
+        uint256 priceOfProject = getPriceOfProject(_id);
+        require(msg.value>=priceOfProject,"not enough for single token");
+
+            projectStructs[_id].projectOwnerShares[_msgSender()] += msg.value/priceOfProject;
+            projectStructs[_id].projectSales += msg.value;
+            projectStructs[_id].projectHolders.push(payable(_msgSender()));
+            _safeTransferFrom(address(this),_msgSender(),_id,msg.value/priceOfProject,'0x0');
         }
-        else {
-            _safeTransferFrom(address(this),_msgSender(),id,1,'0x0');
-            projectOwnerShares[id][_msgSender()]+=1;
+
+
+        function adminTransfer
+        (uint256 _id,
+        uint256 amount,
+        address buyer
+        ) public
+        nonReentrant()
+        onlyRole(DEFAULT_ADMIN_ROLE)
+        {
+            _safeTransferFrom(address(this),buyer,_id,amount,'0x0');
+            projectStructs[_id].projectOwnerShares[_msgSender()] =amount;
+            //projectOwnerShares[id][_msgSender()]+=amount;
         }
-    }
-    function adminTransfer(uint256 id, uint256 amount, address buyer) public onlyRole(DEFAULT_ADMIN_ROLE){
-        _safeTransferFrom(address(this),buyer,id,amount,'0x0');
-        projectOwnerShares[id][_msgSender()]+=amount;
-    }
-    function projectSalesWithdraw(uint256 id) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        require(projectSales[id]>0,"no sales to withdraw");
-        payable(_msgSender()).transfer(projectSales[id]);
-    }
-    function projectProfitDeposit(uint256 id) public payable {
-        projectProfit[id]+=msg.value;
-    }
-    function projectProfitDistribution(uint256 id) public {
-     //   for(uint256 i=0;i<=projectHoldersCount[id];i++){
-     //       projectHolders[i].transfer(projectProfit*totalSupply(id)/projectOwnerShares[id][address])
-     //   }
-    }
+
+
+        function getSalesOfProject(uint256 _id)
+        internal
+        view
+        returns(uint256)
+        {
+            return projectStructs[_id].projectSales;
+        }
+
+        function projectSalesWithdraw(uint256 _id) 
+        public
+        onlyRole(DEFAULT_ADMIN_ROLE)
+        nonReentrant()
+        {
+           uint256 salesOfProject = getSalesOfProject(_id);
+           require(salesOfProject>0,"no sales to withdraw");
+           payable(_msgSender()).transfer(salesOfProject);
+        }
+
+        function projectProfitDeposit(uint256 _id)
+        public
+        payable {
+            uint256 deposit = msg.value;
+            for (uint i=0;i<=projectStructs[_id].projectHolders.length;i++){
+                uint256 ownerPercentage = projectStructs[_id].projectOwnerShares[projectStructs[_id].projectHolders[i]]/projectStructs[_id].projectTotalSupply;
+                payable(projectStructs[_id].projectHolders[i]).transfer(deposit*ownerPercentage);
+            }
+            //getProjectGainsOfHolder()
+            // projectStructs[_id].projectProfit = deposit;
+            //projectStructs[_id].profitGainsByOwner[msg.sender] += 
+        }
+
+
+        function projectProfitDistributionByOwnerCall
+        (uint256 _id)
+        public
+        {
+            bool getProof = getProofOfHold(_id);
+            require(getProof,"You are not holder project");
+            uint256 profitByCaller = getProjectOwnerSharesByOwnerCall(_id);
+            payable(msg.sender).transfer(getProjectProfitById(_id)*totalSupply(_id)/profitByCaller);
+        }
+
+//----------------------------------------- STRUCT INTERACTION --------------------------------------------------------------
+
+
+
+        function getGainsOfHolder(address, uint256) internal {
+
+
+
+        }
+
+        function getPriceOfProject(uint256 _id)
+        internal
+        view
+        returns(uint256) {
+        return projectStructs[_id].projectPrice;
+        }
+
+
+
+
+        function getProofOfHold(uint256 _id)
+        internal
+        view
+        returns(bool)
+        {
+        return projectStructs[_id].addressProjectHolder[_msgSender()];
+        }
+
+        function getHoldersById(uint256 _id)
+        internal
+        view
+        returns(address payable[] memory)
+        {
+            return projectStructs[_id].projectHolders;
+        }
+
+        function getProjectProfitById(uint256 _id)
+        internal
+        view
+        returns(uint256)
+        {
+        return projectStructs[_id].projectProfit;
+        }
+
+        function getProjectOwnerSharesByOwnerCall(uint256 _id)
+        internal
+        view
+        returns(uint256)
+        {
+            return projectStructs[_id].projectOwnerShares[_msgSender()];
+        }
 
 
     function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
@@ -97,4 +256,5 @@ contract ProjectCrowdfunding is ReentrancyGuard, ERC1155,ERC1155Holder, AccessCo
         return super.supportsInterface(interfaceId);
     }
 
-}
+    }
+    
